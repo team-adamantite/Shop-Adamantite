3eef55d9ab1cfb6a54550b2b0603f1afc0d81431
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-13 16:44:38 -0600

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return <div>hello from react</div>;
   }
