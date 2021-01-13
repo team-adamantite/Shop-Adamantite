3fa33dad8d46012852c1df33c6abc86f6e40df19
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-13 11:51:15 -0600

    made changes for some folder bugs, and Router bugs

---
+++ server/Routes/router.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router(); // cerate a new router obj
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const port = 3000;
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const PORT = 3000;
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 // server routing
-app.use('/api/endpoint', require('./Routes/index.js'));
+app.use('/api', require('./Routes/router'));
 
 // listening
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
