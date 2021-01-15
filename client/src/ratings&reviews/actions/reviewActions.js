84d20c90f5cf844221745e0d372af5fd03d68927
Chris Thackrey <c.r.thackrey@gmail.com>
2021-01-14 19:21:03 -0600

    update Reviews and Ratings components

--- client/src/ratings&reviews/actions/reviewActions.js
+++ client/src/ratings&reviews/actions/reviewActions.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 const TEMP_CONSTANT = 'TEMP_CONSTANT';
-export const createProductReview = () => {};
+export const createProductReview = () => {
+  // axios.get().then(dispatch({ACTION_TYPE, payload: res.data}))
+};
--- client/src/store.js
+++ client/src/store.js
@@ -36,7 +36,7 @@ const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  initialState,
+  {},
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
