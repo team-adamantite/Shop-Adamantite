07b6bf3658b50447e0d8c1a5cf6b282a28e99aa8
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-18 12:49:01 -0600

    update features for questions and answers widget

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ProductRatings from './reviews/components/ProductRatings';
-import QAcontainer from './questions&answers/components/main';
+import QAcontainer from './questions/components/main';
 import OverviewContainer from './overview/components/OverviewMain';
 
 export default class App extends Component {
@@ -9,7 +9,7 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div id='main'>
+      <div id="main">
         {/* Placeholder for ProductOverview Component: */}
         <div className="container m-2">
           <div className="card border-1 border-dark text-center py-3">
@@ -18,18 +18,14 @@ export default class App extends Component {
           </div>
         </div>
         <ProductRatings />
-        {/* Placeholder for Questions&Answers Component: */}
-        <div className='container m-2'>
-          <div className='card border-1 border-dark text-center py-3'>
-            <h5 className='card-title'>
-              <QAcontainer />
-            </h5>
-          </div>
-        </div>
+        {/* Placeholder for Questions Component: */}
+
+        <QAcontainer />
+
         {/* Placeholder for RelatedItems&Comparison Component: */}
-        <div className='container m-2'>
-          <div className='card border-1 border-dark text-center py-3'>
-            <h5 className='card-title'>Related Items & Comparison</h5>
+        <div className="container m-2">
+          <div className="card border-1 border-dark text-center py-3">
+            <h5 className="card-title">Related Items & Comparison</h5>
           </div>
         </div>
       </div>
--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-<<<<<<< HEAD
-import { productReviewReducer } from './ratings&reviews/reducers/productReviewReducers.js';
-import { product, styles } from './dummyData.js';
-=======
 import productReviewReducer from './reviews/reviewReducers/reviewsReducer';
-import { product } from './dummyData.js';
->>>>>>> 28455ed630e10e1679d1f408a2e987b71b4859b2
+import { product, styles } from './dummyData.js';
 
 //action
 var changeCurrentProduct = (product) => ({
   type: 'CHANGE_PRODUCT',
-  payload: product
+  payload: product,
 });
 //reducer
 var currentProductReducer = (state = { currentProduct: {} }, action) => {
@@ -36,13 +31,12 @@ var productStylesReducer = (state = { productStyles: [] }, action) => {
 const rootReducer = combineReducers({
   currentProduct: currentProductReducer,
   productStyles: productStylesReducer,
-  reviews: productReviewReducer
+  reviews: productReviewReducer,
 });
 
 const store = createStore(
   rootReducer,
-  { currentProduct: product,
-    productStyles: styles},
+  { currentProduct: product, productStyles: styles },
   composeWithDevTools(applyMiddleware(thunk))
 );
 
