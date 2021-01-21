c4f71577cc5a1d6546995745c8c93871977b5e47
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-21 17:02:04 -0600

    update with gallery carousel

--- client/src/overview/components/gallery.js
+++ client/src/overview/components/gallery.js
@@ -1,41 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import ImageGallery from 'react-image-gallery';
 
 var gallery = ({ styles, currentStyle }) => {
   console.log('what is the photo', currentStyle.photos);
-  return (
-    <span className="gallery">
-      <Carousel
-        width={'450px'}
-        height={'400px'}
-        infiniteLoop={true}
-        thumbWidth={'70px'}
-      >
-        {currentStyle.photos &&
-          currentStyle.photos.map((photo, index) => {
-            return (
-              <div
-                style={{
-                  height: '500px',
-                  width: '500px',
-                  backgroundColor: 'pink',
-                }}
-                key={index}
-              >
-                <img
-                  className={'galleryPic'}
-                  style={{ height: '100%', width: '100%' }}
-                  src={photo.url}
-                />
-                <p className="legend">photo {index}</p>
-              </div>
-            );
-          })}
-      </Carousel>
-    </span>
-  );
+
+  if (!currentStyle.photos) {
+    return null;
+  } else {
+    var newImages = [];
+    for (let image of currentStyle.photos) {
+      newImages.push({
+        original: image.url,
+        thumbnail: image.thumbnail_url,
+      });
+    }
+    console.log('newimage', newImages);
+    return (
+      <div style={{ width: '55%', height: 'auto' }}>
+        <ImageGallery
+          items={newImages}
+          thumbnailPosition={'left'}
+          showBullets={true}
+          autoPlay={true}
+          slideInterval={3000}
+        />
+      </div>
+    );
+  }
 };
 
 var mapStateToProps = (state) => ({
