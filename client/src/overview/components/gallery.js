4c9435b6e1d455d4336723840dc6c303f540ecac
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-21 11:41:26 -0600

    update

--- client/src/overview/components/gallery.js
+++ client/src/overview/components/gallery.js
@@ -1,30 +1,47 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { connect } from 'react-redux';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-var gallery = ({styles, currentStyle}) => {
-  console.log('what is the photo', currentStyle.photos)
+var gallery = ({ styles, currentStyle }) => {
+  console.log('what is the photo', currentStyle.photos);
   return (
-    <span className = 'gallery'>
-    <Carousel width = {'450px'} height = {'400px'}infiniteLoop={true} thumbWidth={"70px"}>
-      {currentStyle.photos && currentStyle.photos.map((photo, index) => {
-        return (
-          <div style = {{height: '500px', width: '500px', backgroundColor: 'pink'}} key={index}>
-            <img className = {'galleryPic'} style = {{height: '100%', width: '100%'}} src={photo.url} />
-            <p className="legend">photo {index}</p>
-          </div>
-        )
-      })}
-    </Carousel>
+    <span className="gallery">
+      <Carousel
+        width={'450px'}
+        height={'400px'}
+        infiniteLoop={true}
+        thumbWidth={'70px'}
+      >
+        {currentStyle.photos &&
+          currentStyle.photos.map((photo, index) => {
+            return (
+              <div
+                style={{
+                  height: '500px',
+                  width: '500px',
+                  backgroundColor: 'pink',
+                }}
+                key={index}
+              >
+                <img
+                  className={'galleryPic'}
+                  style={{ height: '100%', width: '100%' }}
+                  src={photo.url}
+                />
+                <p className="legend">photo {index}</p>
+              </div>
+            );
+          })}
+      </Carousel>
     </span>
-  )
-}
+  );
+};
 
-var mapStateToProps = (state) => (
-  {styles: state.productStyles,
-  currentStyle: state.currentStyle}
-);
+var mapStateToProps = (state) => ({
+  styles: state.productStyles,
+  currentStyle: state.currentStyle,
+});
 
 var galleryContainer = connect(mapStateToProps, null)(gallery);
 
