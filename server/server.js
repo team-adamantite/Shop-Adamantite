8e76ae55e728fe2dee5a4d8f2798ad0be321de21
scbrazil <seancbrazil@gmail.com>
2021-01-22 15:21:55 -0600

    Yet more merge conflicts on local branch.

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const proxy = require('express-http-proxy');
+const PORT = 3000;
+const cors = require('cors');
+const path = require('path');
+const sharp = require('sharp');
+const compression = require('compression');
+>>>>>>> main
+
+const app = express();
+
+// console.log('this is the token ', token)
+
+// middleware
+// compress all responses
+app.use(compression());
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+app.use(
+  '/proxy',
+  proxy('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/')
+);
+
+// listening
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
