df3bf2a0b93da200a173077ebd7fd309cb273b4e
ShinanSun <shinansun0516@gmail.comgit config --global user.email shinansun0516@gmail.com>
2021-01-22 20:54:51 -0600

    deleted all messed testing suites and updated component of question.js

--- client/src/questions/components/maintest.js
+++
@@ -1,5 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { mount } from 'enzyme';
-import App from '../../App';
-Enzyme.configure({ adapter: new Adapter() });
